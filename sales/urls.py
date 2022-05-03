fb2f2edf8e890b7d214cf458f50099e579d571c4
iakev <kirimikmwongera@gmail.com>
2022-05-03 14:01:38 +0300

    Just a minor change to allow standardization of urls paths declared

--- sales/urls.py
+++ sales/urls.py
@@ -6,5 +6,5 @@ from sales import views
 urlpatterns = [
     path('sales/',views.SalesList.as_view()),
     path('sales/receipts/',views.ProductsSalesList.as_view()),
-    path('sales/<id>',views.SaleDetailView.as_view())
+    path('sales/<id>/',views.SaleDetailView.as_view())
 ]
