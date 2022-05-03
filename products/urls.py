b867773cd22602aa6e9f766ba2d51e03b9e400b6
iakev <kirimikmwongera@gmail.com>
2022-05-03 14:00:17 +0300

    Added url that connects to product view corresponding to passed in product id

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('categories/',views.CategoriesList().as_view()),
     path('products/',views.ProductsList.as_view()),
     path('categories/<slug:category_slug>/',views.CategoryProducts.as_view()),
-    path('products/search/',views.search)
+    path('products/search/',views.search),
+    path('products/<id>/',views.ProductRetrieval.as_view())
 ]
