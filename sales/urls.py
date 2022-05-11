f1848236835fb95e8733af0537399bb4fbaee36b
iakev <kirimikmwongera@gmail.com>
2022-05-11 09:37:19 +0300

    Added corresponding url to the created ProductSalesDetailView

--- sales/urls.py
+++ sales/urls.py
@@ -6,5 +6,6 @@ from sales import views
 urlpatterns = [
     path('sales/',views.SalesList.as_view()),
     path('sales/receipts/',views.ProductsSalesList.as_view()),
-    path('sales/<id>/',views.SaleDetailView.as_view())
+    path('sales/<id>/',views.SaleDetailView.as_view()),
+    path('sales/receipts/<id>/',views.ProductSalesDetailView.as_view())
 ]
