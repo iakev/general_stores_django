79d150082022c5470d86f462e5023ae434323280
iakev <kirimikmwongera@gmail.com>
2022-05-18 11:48:17 +0300

    Added a new sale url to match with create new sale view

--- sales/urls.py
+++ sales/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('sales/',views.SalesList.as_view()),
     path('sales/receipts/',views.ProductsSalesList.as_view()),
     path('sales/<id>/',views.SaleDetailView.as_view()),
-    path('sales/receipts/<id>/',views.ProductSalesDetailView.as_view())
+    path('sales/receipts/<id>/',views.ProductSalesDetailView.as_view()),
+    path('new_sale',views.CreateSaleView.as_view())
 ]
